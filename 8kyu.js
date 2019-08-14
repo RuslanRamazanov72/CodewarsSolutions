084561518bdcbbda1dad67b54360c70b2cada5b3
RuslanRamazanov72 <@Polina31051986>
2019-08-13 21:20:04 -0400

    Added new solution

--- 8kyu.js
+++ 8kyu.js
@@ -19,3 +19,17 @@ function countSheeps(arrayOfSheep) {
     }
     return count
 }
+
+// [Squash the bugs]
+function findLongest(str) {
+
+    let spl = str.split(" ");
+    let longest = 0;
+
+    for (let i = 0; i < spl.length; i++) {
+        if (spl[i].length > longest) {
+            longest = spl[i].length;
+        }
+    };
+    return longest;
+}
