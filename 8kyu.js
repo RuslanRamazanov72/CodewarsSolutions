cc6e11e1c1ce4b2a641b8721627315aa36876af6
RuslanRamazanov72 <@Polina31051986>
2019-08-13 21:14:40 -0400

    Changes in file

--- 8kyu.js
+++ 8kyu.js
@@ -1,5 +1,5 @@
 
-// Simple Palindrome
+// [Simple Palindrome]
 function isPaKlindrome(line) {
 
     for (let i = 0; i < line.length/2; i++) {
@@ -9,7 +9,7 @@ function isPaKlindrome(line) {
     }
 }
 
-// Counting sheep
+// [Counting sheep]
 function countSheeps(arrayOfSheep) {
     let count = 0;
     for (let i = 0; i < arrayOfSheep.length; i++) {
